da5e2f473bbadcb31ba99897ff6ea2097c5723cc
Nataliya Chechkova <sotsgorod2@yandex.ru>
2021-11-13 18:00:56 +0300

    Example011_voidFillArray

---
+++ Example011_voidFillArray/Program.cs
@@ -0,0 +1,13 @@
+﻿void FillArray (int[]collection);
+{
+    int Length = collection.Length;
+    int index = 0;
+    while(index<Length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+int[]array = new int(10);
+
+Console.WriteLine("Collection[index]");

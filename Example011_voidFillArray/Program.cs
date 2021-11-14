e02893f252212635d26085d37a42ae70df53ae63
Nataliya Chechkova <sotsgorod2@yandex.ru>
2021-11-14 09:30:34 +0300

    Example011_voidFillArray

--- Example011_voidFillArray/Program.cs
+++ Example011_voidFillArray/Program.cs
@@ -1,13 +1,27 @@
-﻿void FillArray (int[]collection);
+﻿void FillArray(int[] collection);
 {
-    int Length = collection.Length;
+    int length = collection.Length;
     int index = 0;
-    while(index<Length)
+    while (index < length)
     {
-        collection[index] = new Random().Next(1,10);
+        collection[index] = new Random().Next(1, 10);
         index++;
     }
 }
-int[]array = new int(10);
 
-Console.WriteLine("Collection[index]");
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+         Console.WriteLine(col[position]);
+         position++;
+    }
+
+}
+int[] array = new int[10]; // Создай новый массив, в котором 10 элементов
+
+FillArray(array);
+PrintArray(array);
+//Console.WriteLine("Collection[index]");

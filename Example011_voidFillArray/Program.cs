39558b5c4ac56e3c16fd82d2d50f104d2b29c4e7
Nataliya Chechkova <sotsgorod2@yandex.ru>
2021-11-14 10:06:05 +0300

    Example011_voidFillArray

--- Example011_voidFillArray/Program.cs
+++ Example011_voidFillArray/Program.cs
@@ -1,10 +1,10 @@
-﻿void FillArray(int[] collection);
+﻿void FillArray(int[] collection);//дан массив целых чисел, под названием "collection"
 {
-    int length = collection.Length;
+    int length = collection.Length; //длина = длине массива "collection"
     int index = 0;
-    while (index < length)
+    while (index < length) //пока индекс элемента < длины массива
     {
-        collection[index] = new Random().Next(1, 10);
+        collection[index] = new Random().Next(1, 10); //элемент - новое рандомное число от 1 до 10
         index++;
     }
 }
@@ -20,8 +20,33 @@ void PrintArray(int[] col)
     }
 
 }
-int[] array = new int[10]; // Создай новый массив, в котором 10 элементов
+// int[] array = new int[10]; // Создай новый массив, в котором 10 элементов
+
+// FillArray(array);
+// PrintArray(array);
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;//если в массиве нет заданного числа, то чтобы это распознать вводят: int position = -1
+
+    while (index < count)
+    {
+        if(collection[index]) == find)
+        {
+        position = index;
+        break;
+        }
+         index++;
+    }
+    return position;
+}
+int[] array = new int[10];
 
 FillArray(array);
 PrintArray(array);
-//Console.WriteLine("Collection[index]");
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
